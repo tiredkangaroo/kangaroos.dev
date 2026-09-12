3ab74bc79a752f14b25976d55f876bbaf4e17055
Ajitesh Kumar <ajinest6@gmail.com>
2026-09-12 18:23:35 -0400

    kangaroos starting?

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <style>
+    :root {
+      --card-bg: #ace8bc;
+      --card-border: #346140;
+    }
+    html,
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: monospace;
+    }
+  </style>
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+  import photos_urls from "$lib/assets/photos_urls.txt?raw";
+
+  const birthday = new Date("2010-02-01");
+  const today = new Date();
+  const age = Math.floor((today.getTime() - birthday.getTime()) / (1000 * 60 * 60 * 24 * 365)); // 1000 ms -> s, 60s -> min, 60 min -> hr -> 24 hr -> day, 365 days -> year
+
+  let photos = photos_urls.split("\n").filter((url) => url.trim() !== ""); // grab out all the urls and filter out empty lines
+</script>
+
+<div class="main">
+  <nav>
+    <!-- <div class="card"> -->
+    <div class="card">
+      <h1 class="my-freaking-name">hi, i'm aji!</h1>
+      <p class="desc">i'm a <span id="age">{age}</span>-year-old from new york!</p>
+      <p><a role="button" href="https://github.com/tiredkangaroo" class="card-button">github</a></p>
+    </div>
+    <!-- </div> -->
+    <div></div>
+  </nav>
+  <h1>photography</h1>
+  <div class="photos">
+    {#each photos as photo}
+      <img src={photo} alt="photo" class="photo" />
+    {/each}
+  </div>
+</div>
+
+<style>
+  nav {
+    /* display: grid; */
+    /* grid-template-columns: 1fr 1fr; */
+    /* gap: 2rem; */
+    width: 100%;
+    /* background-color: #f5f5f5; */
+    /* padding: 1rem; */
+  }
+  .my-freaking-name {
+    font-size: 4rem;
+    font-weight: bold;
+    color: var(--card-border);
+    /* text-align: center; */
+    line-height: 1.7;
+    margin: 0;
+  }
+  .photos {
+    display: grid;
+    grid-template-columns: masonry;
+    grid-template-rows: masonry;
+  }
+  .photo {
+    max-width: 200px;
+  }
+  .desc {
+    font-size: 1.5rem;
+    /* color: var(--card-border); */
+    /* text-align: center; */
+  }
+  .card {
+    background-color: var(--card-bg);
+    border: 1px solid var(--card-border);
+    color: var(--card-border);
+    padding: 0.4rem 1rem;
+  }
+  .card-button {
+    background-color: var(--card-border);
+    text-decoration: none;
+    color: white;
+    padding: 0.4rem 1rem;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 100%;
+    margin: 20px auto;
+    align-items: center;
+    /* background-color: var(--card-bg);
+    border: 1px solid var(--card-border);
+    padding: 1rem; */
+  }
+</style>
